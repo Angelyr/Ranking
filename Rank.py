3aa7276dd272cfed6c6bc3a4dfeac74540c24985
Ktxu18 <kristx18@gmail.com>
2018-11-07 17:06:57 -0500

    Added to Rank

--- Rank.py
+++ Rank.py
@@ -1,1 +1,31 @@
 #Kristine
+
+import datetime
+
+class Rank: 
+    def __init__(self):
+        self.url = ''
+        self.pageRank = 0
+        self.position = 0
+        self.frequency = 0
+        self.section = ''
+        self.lastUpdated = datetime.datetime.now()
+        self.totalRank = 0
+        self.nGram = ''
+
+    def Rank():
+
+    
+    def getPageRank():
+
+
+    def getPositionScore():
+
+    
+    def getFrequencyScore():
+
+    
+    def getSectionScore():
+
+    
+    def getUpdateScore(): 
