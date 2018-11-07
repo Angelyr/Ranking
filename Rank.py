abc9a439099002797f4d180fb44efcd4950f1b58
Ktxu18 <kristx18@gmail.com>
2018-11-07 17:10:58 -0500

    Added to Rank

--- Rank.py
+++ Rank.py
@@ -14,18 +14,23 @@ class Rank:
         self.nGram = ''
 
     def Rank():
-
+        # calculate the total rank score 
     
-    def getPageRank():
+    def getPageRankScore():
+        # get the page rank for this webpage
 
 
     def getPositionScore():
+        # get the position score for the n-gram on this page 
 
     
     def getFrequencyScore():
+        # get the frequency score for the n-gram on this page
 
     
     def getSectionScore():
+        # get the section score for the n-gram on this page 
 
     
     def getUpdateScore(): 
+        # get the update score for this webpage
