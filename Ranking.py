068e22da90f42830f550309faadf49f2210ad3a5
Angel <scardking@gmail.com>
2018-11-08 01:08:05 -0500

    Skeleton for Ranking. Finished TextProcessing

--- Ranking.py
+++ Ranking.py
@@ -1,1 +1,16 @@
 #Angel
+
+print('Ranking is now running')
+rankedList = []
+
+def parseMsg(msg):
+    return
+
+def createRanks(document):
+    return
+
+def combineRanks(ranks):
+    return
+
+def sendUrls():
+    return
---
+++ TextProcessing.py
@@ -0,0 +1,13 @@
+#returns a list of ngrams of sizes 1-5
+def makeNGrams(query):
+    words = query.split(' ')
+    output = []
+    for n in range(1,5):
+        for i in range(len(words)-n+1):
+            output.append(words[i:i+n])
+    return output
+
+#testing
+test = 'How to change the world in 20 days'
+for ngram in makeNGrams(test):
+    print(ngram)
