610b4f5b5c4254f2a6277e0a601e953f15e87206
Brandon <thornb@rpi.edu>
2018-11-08 13:12:11 -0500

    Added a better way to start flask server rather than environment variable. Added pseudocode for Rest MessageReceiver class

--- MessageReceiver.py
+++ MessageReceiver.py
@@ -1,7 +1,47 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import time
+import requests
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+
+# Receives the UI team's query and calls getRanking to get ranking results 
+@app.route('/search', methods=['GET'])
 def recvQuery():
-	print(request.form['query'])
-	return 'Received'
+	print(request.args.get('query'))
+	
+	processQuery(request.args.get('query'))
+	
+	return 'Recvieved your query: ' + request.args.get('query')
+
+
+def sendIndexReq(nGram):
+	r = requests.post('https://httpbin.org/post', data = {'key':'value'})
+	print(r.content)
+	return r
+
+
+# Call functions in other files to do the business logic of ranking
+def getRanking(query):
+	
+	# Call other file to get the n-grams
+	# nGrams = getNgrams(query)
+
+	# for each n-gram, send a query to index
+
+	# Send the nNgrams to the Index team to get the document features
+	# r = sendIndexReq(nNgrams)
+	r = sendIndexReq(query)
+
+	# collect all the nGramFeatures into a data structure (list of dicts?)
+	# nGramFeatures = 
+
+	# sent the result to the ranking class, receive ranked list
+	# rankedList = calculateRanks(nGramFeatures)
+	# return rankedList
+
+
+
+if __name__ == "__main__":
+	# @TODO remove debug before production
+	app.run(debug=True)
