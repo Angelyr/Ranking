adf66545f46ba611d794c4b300bf109ce56387ff
Angel <scardking@gmail.com>
2018-11-12 13:19:54 -0500

    Refactored Ranking into an object so i can implement threads

--- Ranking.py
+++ Ranking.py
@@ -2,53 +2,56 @@ import json
 import threading
 from Rank import Rank
 
-rankedList = []
+class Ranking:
+    def __init__(self):
+        self.rankedList = []
 
-#receives documents from rest layer. Turns it into rank objects. Adds it to rankedList
-def parseMsg(ngram, msg):
-    for item in data["documents"]:
-        rank = Rank(ngram, item["document_id"], item["pagerank"], item["position"], item["frequency"], item["section"], item["date_created"])
-        rankedList.append(rank)
-    return
+    #receives documents from rest layer. Turns it into rank objects. Adds it to rankedList
+    def addDocuments(self, ngram, msg):
+        for item in msg["documents"]:
+            rank = Rank(ngram, item["document_id"], item["pagerank"], item["position"], item["frequency"], item["section"], item["date_created"])
+            self.rankedList.append(rank)
+        return
 
 
-#combines the ranks of documents with the same docID
-def combineRanks():
-    rankedList.sort()
-    for i in range(len(rankedList)-1):
-        if rankedList[i].docID == rankedList[i+1].docID:
-            if(rankedList[i].totalRank > rankedList[i+1].totalRank):
-                rankedList.pop(i+1)
-            else:
-                rankedList.pop(i)
-            continue
-    sendDocuments()
-    return
+    #combines the ranks of documents with the same docID
+    def combineRanks(self):
+        self.rankedList.sort()
+        for i in range(len(self.rankedList)-1):
+            if self.rankedList[i].docID == self.rankedList[i+1].docID:
+                if(self.rankedList[i].totalRank > self.rankedList[i+1].totalRank):
+                    self.rankedList.pop(i+1)
+                else:
+                    self.rankedList.pop(i)
+                continue
+        return
 
-#sends the urls to the message sender
-def sendDocuments():
-    pages = []
-    for doc in rankedList:
-        pages.append({
-            "document_id": doc.docID,
-            "rank": doc.totalRank
-        })
-    pages = {
-        "pages": pages
-    }
-    printJSON(pages)
-    return pages
+    #sends the urls to the message sender
+    def getDocuments(self):
+        self.combineRanks()
+        pages = []
+        for doc in self.rankedList:
+            pages.append({
+                "document_id": doc.docID,
+                "rank": doc.totalRank
+            })
+        pages = {
+            "pages": pages
+        }
+        return pages
 
 #prints JSON in format that is easier to read
 def printJSON(data):
     print(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
     return
 
+#Testing purposes
+def test():
+    data = json.load(open('input.json'))
+    rankings = Ranking()
+    rankings.addDocuments("hello", data)
+    docs = rankings.getDocuments()
+    printJSON(docs)
+    return
 
-fp = open('input.json')
-data = json.load(fp)
-parseMsg("hello",data)
-combineRanks()
-
-#msg = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
-#parseMsg(msg)
+test()
