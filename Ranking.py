e4f712f301b9b29d2c49a77a8f7d3ea22a699c01
Angel <scardking@gmail.com>
2018-11-14 23:12:13 -0500

    Improved comments of Ranking

--- Ranking.py
+++ Ranking.py
@@ -5,23 +5,33 @@ class Ranking:
     def __init__(self):
         self.rankedList = []
 
-    #Turns msg into rank objects. Adds ranks to rankedList
+    #Input: ngram string and JSON message
+    #Output: none
+    #SideEffect: adds rank to rankedList for each document in msg
+    #Purpose: used by MessageHandler to add documents to rank
     def addDocuments(self, ngram, msg):
         for item in msg["documents"]:
             rank = Rank(ngram, item["document_id"], item["pagerank"], item["position"], item["frequency"], item["section"], item["date_created"])
             self.rankedList.append(rank)
         return
 
-    #used to sort rankedList by ID so the ranks can be combined for each id
+    #Input: rank object
+    #Output: the id of the rank object
+    #SideEffect: none
+    #Purpose: used to sort rankedList by ID
     def sortByID(self, rank):
         return rank.docID
 
-    #For each docId in rankedList it removes all but the highest totalRank
-    #It also combines the matching ngrams
+    #Input: none
+    #Output: none
+    #SideEffect: sorts rankedList by id and removes ranks with the id that have lower total rank
+    #Purpose: used to only return one of each document
     def combineRanks(self):
         self.rankedList.sort(key=self.sortByID)
         for i in range(len(self.rankedList)-2):
+            #if the IDs match
             if self.rankedList[i].docID == self.rankedList[i+1].docID:
+                #pop the rank with the lower totalRank and combine the nGrams
                 if(self.rankedList[i].totalRank > self.rankedList[i+1].totalRank):
                     self.rankedList[i].addNgram(self.rankedList[i+1].nGram)
                     self.rankedList.pop(i+1)
@@ -31,7 +41,10 @@ class Ranking:
                 continue
         return
 
-    #returns the docID and totalRank for each document
+    #Input: none
+    #Output: docID, totalRank, and keywords for each document
+    #SideEffect: effects of combineRanks and lists sorted by total rank
+    #Purpose: used by message handler to get the list of documents to send to UI
     def getDocuments(self):
         self.combineRanks()
         self.rankedList.sort(reverse=True)
@@ -47,7 +60,10 @@ class Ranking:
         }
         return pages
 
-#prints JSON in format that is easier to read
+#Input: JSON string
+#Output: none
+#SideEffect: prints JSON string
+#Purpose: prints JSON in format that is easier to read
 def printJSON(data):
     print(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
     return
@@ -60,5 +76,4 @@ def test():
     docs = rankings.getDocuments()
     printJSON(docs)
     return
-
 #test()
