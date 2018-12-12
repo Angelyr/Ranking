cd545f1b3bef0f6ba13462dedbf8440d7ad28463
Angel <scardking@gmail.com>
2018-12-12 17:53:40 -0500

    TextProcessing tests

--- TextProcessing.py
+++ TextProcessing.py
@@ -7,21 +7,44 @@ def removePuctuation(word):
 
 #returns a list of ngrams of sizes 1-5 with and without punctuation
 def makeNGrams(query):
-    for word in removePuctuation(query).split():
-        if word not in query.split():
-            query += " " + word
-
+    if query is None: return []
+    query += " " + removePuctuation(query)
     words = query.split()
     output = []
-    for n in range(1,5):
+    for n in range(1,6):
         for i in range(len(words)-n+1):
             output.append(words[i:i+n])
     return output
 
 #testing
 def test():
-    test = 'How to change the world? in 20.5 days'
-    for ngram in makeNGrams(test):
-        print(ngram)
+    print("test 1:")
+    test = ''
+    print(makeNGrams(test))
 
-test()
+    print("test 2:")
+    test = '     '
+    print(makeNGrams(test))
+
+    print("test 3:")
+    print(makeNGrams(None))
+
+    print("test 4:")
+    test = 'A'
+    print(makeNGrams(test))
+
+    print("test 5:")
+    test = 'A B C D E'
+    print(makeNGrams(test))
+
+    print("test 6:")
+    test = 'A B C D E F G H I'
+    print(makeNGrams(test))
+
+    print("test 7:")
+    test = 'A? B! C@ D$ E%'
+    print(makeNGrams(test))
+
+    print("test 8:")
+    test = 'How to change the world? in 20.5 days'
+    print(makeNGrams(test))
